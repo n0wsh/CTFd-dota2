1f93d9be7babb82cb832d88442488363d4b3f8b0
ekhangal <e.khangall@gmail.com>
2023-11-15 22:19:42 +0800

    feat(scoreboard): add first blood sound

---
+++ scoreboard/src/server/firstblood.ts
@@ -0,0 +1,29 @@
+import { APIResponse } from "./types";
+
+export type FirsBloodItem = {
+  challenge_id: number;
+  team_id: number;
+};
+
+export async function fetchFirstBlood() {
+  try {
+    const result: APIResponse<FirsBloodItem[]> = await fetch(
+      `${process.env.CTFD_API_URL}/valorant/first-blood`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CTFD_API_KEY}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    if (result.data) {
+      return result.data;
+    } else {
+      throw new Error("Response data is missing.");
+    }
+  } catch (error) {
+    console.error("Error fetching agent picks:", error);
+    throw error;
+  }
+}
